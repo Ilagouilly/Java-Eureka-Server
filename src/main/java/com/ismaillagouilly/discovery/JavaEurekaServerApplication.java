a158c4cfb010a8f71629cdad20968f0ee538032c
closeupapp <closeupapp@gmail.com>
2021-04-02 20:07:23 +0200

    [ILA] First Commit

---
+++ src/main/java/com/ismaillagouilly/discovery/JavaEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ismaillagouilly.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class JavaEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ismaillagouilly/discovery/JavaEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismaillagouilly.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
